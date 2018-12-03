183a3614c22d99a03585197c8a485fe236cddcce
Dominic M <dominicccc@gmail.com>
2018-12-03 14:45:46 -0800

    script correctly creates table

--- app-guest-list/database/database.js
+++ app-guest-list/database/database.js
@@ -4,17 +4,22 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require(__dirname + '/../../knexfile.js')[environment];
 const knex = require('knex')(configuration);
 
-knex.schema.dropTableIfExists('events');
-.then(() => {
-  knex.schema.createTable('events', (table) => {
-    table.increments('id');
-    table.string('name');
-    table.string('location');
-    table.string('date');
-    table.string('startTime');
-    table.string('endTime');
-    table.timestamps();
+knex.schema.dropTableIfExists('events')
+  .then(() => {
+    console.log('dropped events table');
   })
+  .error((error) => {
+    console.error(error);
+  })
+
+knex.schema.createTable('events', (table) => {
+  table.increments('id');
+  table.string('name');
+  table.string('location');
+  table.string('date');
+  table.string('startTime');
+  table.string('endTime');
+  table.timestamps();
 })
 .then(() => {
   console.log('created events table');
