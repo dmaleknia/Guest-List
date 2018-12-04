cb798cd5f5a08432e661b52c0c2dbbe07bab9f14
Dominic M <dominicccc@gmail.com>
2018-12-04 13:57:27 -0800

    working on RSVP component; added react-numeric-input

--- app-guest-list/react-client/src/components/RSVP.jsx
+++ app-guest-list/react-client/src/components/RSVP.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { useState } from 'react';
+import NumericInput from 'react-numeric-input';
 
 const RSVP = (props) => {
 
-  const [name, setName] = useState();
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
   const [email, setEmail] = useState();
+  const [guests, setGuests] = useState();
 
   return (
     <span>
       RSVP to: {props.eventName}<br />
-      Name <input value={name} onChange={setName}></input><br />
-      Email <input value={email} onChange={setEmail}></input>
+      First Name: <input value={firstName} onChange={setFirstName}></input><br />
+      Last Name: <input value={lastName} onChange={setLastName}></input><br />
+      Email: <input value={email} onChange={setEmail}></input><br />
+      Guests: <NumericInput min={0} max={4} value={guests} onChange={setGuests} />
     </span>
   );
 }
