eaf24a1fa2ccd398dd939323c4523c06c6fd340c
Dominic M <dominicccc@gmail.com>
2018-12-04 14:18:31 -0800

    added API request to RSVP component

--- app-guest-list/react-client/src/components/RSVP.jsx
+++ app-guest-list/react-client/src/components/RSVP.jsx
@@ -22,6 +22,24 @@ const RSVP = (props) => {
     setEmail(event.target.value);
   }
 
+  const submitRSVP = () => {
+    console.log(firstName, lastName, email, guests);
+    axios.post('/rsvps', {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      guests: guests,
+      eventID: eventID
+    })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    setView('postsubmit');
+  }
+
   const renderView = () => {
     if (view === 'postsubmit') {
       return (
@@ -36,7 +54,8 @@ const RSVP = (props) => {
           First Name: <input value={firstName} onChange={handleFirstNameChange}></input><br />
           Last Name: <input value={lastName} onChange={handleLastNameChange}></input><br />
           Email: <input value={email} onChange={handleEmailChange}></input><br />
-          Guests: <NumericInput min={0} max={4} value={guests} onChange={setGuests} />
+          Guests: <NumericInput min={0} max={4} value={guests} onChange={setGuests} /><br />
+          <button id='submitRSVP' onClick={submitRSVP}>Submit RSVP</button>
         </span>
       );
     }
